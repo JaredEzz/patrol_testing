67c5e833f0359a636429dd484b52d8b62e42d2f9
eric-odonoghue <eric.odonoghue@ostrichcyber-risk.com>
2026-01-22 15:19:46 -0700

    init stuff

--- patrol_test/app_test.dart
+++ patrol_test/app_test.dart
@@ -4,7 +4,7 @@ import 'package:patrol/patrol.dart';
 import 'package:patrol_testing/main.dart';
 
 void main() {
-  PatrolBinding.ensureInitialized(PlatformAutomator());
+  PatrolBinding.ensureInitialized();
   patrolTest(
     'counter increments correctly when tapped',
     ($) async {
